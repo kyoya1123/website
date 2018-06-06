c9ecae95375f1424d37b9bfaf802bb2a8686f335
Kyoya Yamaguchi <kyoryu32@gmail.com>
2018-06-07 00:27:30 +0900

    aaa

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php require("index.html"); ?>
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="stylesheet.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="main">
+      <h1>
+        <?php
+          echo "できた";
+          for ($i = 0; $i <= 1000; $i++) {
+            echo "あ";
+          }
+        ?>
+    </h1>
+    </div>
+  </body>
+</html>
